9345f50ae15e1702ba3ab25a2e94b6fea4742933
fabiyeen <163744056+fabiyeen@users.noreply.github.com>
2025-03-03 16:13:04 +0700

    step 8 1

--- app/src/main/java/com/example/jetnews/ui/article/PostContent.kt
+++ app/src/main/java/com/example/jetnews/ui/article/PostContent.kt
@@ -132,8 +132,9 @@ private fun PostHeaderImage(post: Post) {
 @Composable
 private fun PostMetadata(metadata: Metadata) {
     val typography = MaterialTheme.typography
-    Row {
+    Row(Modifier.semantics(mergeDescendants = true) {}) {
         Image(
+
             imageVector = Icons.Filled.AccountCircle,
             contentDescription = null,
             modifier = Modifier.size(40.dp),
